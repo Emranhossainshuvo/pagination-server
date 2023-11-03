e1560e586f06977dcfbf74409ab85fa1f771a74a
Shuvo <shuvoorba28@gmail.com>
2023-11-03 10:42:36 -0700

    products counts in number

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use(express.json());
 
 
 
-
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0fn8ke9.mongodb.net/?retryWrites=true&w=majority`;;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,6 +34,11 @@ async function run() {
     app.get('/products', async(req, res) => {
         const result = await productCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/productsCount', async(req, res) =>{
+      const count = await productCollection.estimatedDocumentCount(); 
+      res.send({count})
     })
 
     // Send a ping to confirm a successful connection
